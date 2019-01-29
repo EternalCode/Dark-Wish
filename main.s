b76613564f238b92a196511e708ba7bf70a7459f
EternalCode <dilshan.gunasekera@mail.utoronto.ca>
2019-01-29 14:49:29 -0500

    imported dexnav

--- main.s
+++ main.s
@@ -5,6 +5,11 @@
         .include "patches/save.s"
         .include "patches/sideways_stairs.s"
         .include "patches/triple_layer_block.s"
+        .include "patches/overworld_cave_effect.s"
+        .include "patches/start_menu.s"
+        .include "patches/select_button.s"
+        .include "patches/hidden_abilities.s"
+        .include "patches/pokemon_bst.s"
 
 		.org 0x08FFFFFF
 			.fill 0xFFFFFF, 0xFF
