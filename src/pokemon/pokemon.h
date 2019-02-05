315d84d50549faf4a19e9fc2ee2b28d81c4cfb98
EternalCode <dilshan.gunasekera@mail.utoronto.ca>
2019-02-05 01:41:18 -0500

    replace old linked C array with incbin
    This is output from generate_tables.py. Should really just have them in gitignore, but I haven't bothered to add the script to the makefile

--- src/pokemon/pokemon.h
+++ src/pokemon/pokemon.h
@@ -3,10 +3,17 @@
 
 #include <pokeagb/pokeagb.h>
 #include "pokemon_base_stats.h"
-#include "pokebacks.h"
-#include "pokefronts.h"
+
+#define START_ADDR_FRONT_T (const u8*)0x08740000
+#define START_ADDR_BACK_T (const u8*)0x8740000 + 0x4000
+#define START_ADDR_PAL_T (const u8*)0x8740000 + 0x8000
 
 extern const u8 gSpeciesNames[][POKEMON_NAME_LENGTH + 1];
 extern const u16 *const gLevelUpLearnsets[SPECIES_MAX];
+extern const void* FrontSpriteGfxTable;
+extern const void* BackSpriteGfxTable;
+extern const void* PKMNSpritePalTable;
+
+
 
 #endif /* POKEMON_H_ */
