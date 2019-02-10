92070307523e2b2e6e313968c167294656ea3da8
EternalCode <dilshan.gunasekera@mail.utoronto.ca>
2019-02-10 08:18:27 -0500

    Dexnav select button hook removed

--- src/dexnav/keypad/select_button.c
+++ src/dexnav/keypad/select_button.c
@@ -3,22 +3,16 @@
 
 extern void init_dexnav_hud(u16 species, u8 environment);
 
-
-u8 select_hook_restore()
+u8 CheckRegisteredDexnavMon()
 {
-    script_env_init_script((void*) 0x81A77A0); // script that lets you know you can register item via select
-    return 0;
-}
-
-u8 check_registered_select() {
     u16 var_val = VarGet(DEXNAV_VAR);
     if (var_val) {
         if (task_is_running(dexnav_hud_manage)) {
             // don't redo the search...
-            return 0;
+            return false;
         }
         init_dexnav_hud(var_val & 0x7FFF, var_val >> 15);
-        return 0;
+        return true;
     }
-    return 1;
+    return false;
 }
--- src/hooks/dexnav_hooks.s
+++ src/hooks/dexnav_hooks.s
@@ -1,20 +1,3 @@
-@ This hook adds a checks determining if the select button is bound to a Pokemon
-.thumb
-.align 2
-.global check_dexnav_select
-
-check_dexnav_select:
-    bl check_registered_select
-    cmp r0, #0x0
-    bne restore
-    ldr r1, =(0x810ADA0|1)
-    bx r1
-
-restore:
-   bl select_hook_restore
-   ldr r1, =(0x810ADA0|1)
-   bx r1
-
 @--------------------------------------------
 @ The vanilla startmenu enters a fadescreen on certain options
 @ This hook adds an exception to that fade for Poketools
