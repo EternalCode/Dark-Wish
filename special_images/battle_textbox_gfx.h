b048089f6c150e8fe499f03055562c223b71e922
EternalCode <dilshan.gunasekera@mail.utoronto.ca>
2019-02-13 15:59:54 -0500

    Port battle engine images

---
+++ special_images/battle_textbox_gfx.h
@@ -0,0 +1,12 @@
+#ifndef BATTLE_TEXTBOX_GFX_H_
+#define BATTLE_TEXTBOX_GFX_H_
+
+#include <pokeagb/pokeagb.h>
+
+#include "battle_textbox.h"
+#include "battle_textbox_action_select.h"
+#include "battle_textbox_move_select.h"
+#include "bbox.h"
+
+
+#endif /* BATTLE_TEXTBOX_GFX_H_ */
---
+++ special_images/generator.sh
@@ -0,0 +1,7 @@
+grit battle_textbox.png battle_textbox_action_select.png battle_textbox_move_select.png -fh -ftc -g -gS -gzl -gB4 -gu8 -m -mp5 -mu8 -p -pe16 -pS -pu8 -ah256 -aw256 -O bbox
+cp *.h ../src/battle_text
+cp *.c ../src/battle_text
+cp battle_textbox_gfx.h btg.d
+rm -rf *.h
+rm -rf *.c
+mv btg.d battle_textbox_gfx.h 
