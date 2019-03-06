06fd038fa3054f221474353b27473e934651c9ef
Evan <eroelke@gmail.com>
2019-03-05 17:59:49 -0700

    remove old brm dependence

---
+++ src/fieldMoves/brmText.c
@@ -0,0 +1,83 @@
+
+#include <pokeagb/pokeagb.h>
+
+const pchar summaryString[] = _("Summary");
+const pchar switchString[] = _("Switch");
+const pchar cancelString[] = _("Cance");
+const pchar itemString[] = _("Item");
+const pchar giveString[] = _("Give");
+const pchar takeString[] = _("Take");
+const pchar mailString[] = _("Mail");
+const pchar readString[] = _("Read");
+const pchar shiftString[] = _("Shift");
+const pchar sendOutString[] = _("Send Out");
+const pchar enterString[] = _("Enter");
+const pchar noEntryString[] = _("No Entry");
+const pchar storeString[] = _("Store");
+const pchar registerString[] = _("Register");
+const pchar tradeString[] = _("Trade");
+const pchar flashString[] = _("Flash");
+const pchar cutString[] = _("Cut");
+const pchar flyString[] = _("Fly");
+const pchar strengthString[] = _("Strength");
+const pchar surfString[] = _("Surf");
+const pchar rockSmashString[] = _("Rock Smash");
+const pchar waterfallString[] = _("Waterfall");
+const pchar teleportString[] = _("Teleport");
+const pchar digString[] = _("Dig");
+const pchar milkDrinkString[] = _("Milk Drink");
+const pchar softboiledString[] = _("Softboiled");
+const pchar sweetScentString[] = _("Sweet Scent");
+const pchar diveString[] = _("Dive");
+
+const pchar fieldMoveErrorMsg[] = _("Can’t use this move right now!\p");
+const pchar FlashErrorMsg[] = _("Flash can’t be used here!\p");
+const pchar CutErrorMsg[] = _("There is no grass within range!\p");
+const pchar FlyErrorMsg[] = _("You can’t fly from\nthis location!\p");
+const pchar StrengthErrorMsg[] = _("There are no boulders to\npush here!\p");
+const pchar SurfErrorMsg[] = _("Unable to Surf here!\p");	// differentiate between trying on land vs. fast current?
+const pchar RockSmashErrorMsg[] = _("There are no rocks to\nsmash here!\p");
+const pchar WaterfallErrorMsg[] = _("There is no waterfall\nto climb!\p");
+const pchar TeleportErrorMsg[] = _("Can’t Teleport from here!\p");
+const pchar DigErrorMsg[] = _("Can’t Dig from this location!\p");
+const pchar MilkDrinkErrorMsg[] = _("There are no Pokémon to heal\nwith Milk Drink!\p");
+const pchar SoftboiledErrorMsg[] = _("There are no Pokémon to heal\nwith Softboiled!\p");
+const pchar SweetScentErrorMsg[] = _("Can’t use Sweet Scent at this location!\p");
+const pchar DiveErrorMsg[] = _("There is no deep water here!\p");
+
+const pchar AskTeleport[] = _("Teleport back to\n{STR_VAR_1}?");
+const pchar AskDig[] = _("Dig through to return\nto {STR_VAR_1}?");
+const pchar AskDive[] = _("Dive into the deep water?");
+
+const pchar summaryDescription[] = _("View stats and moves.");
+const pchar switchDescription[] = _("Switch pokémon order.");
+const pchar cancelDescription[] = _("Cancel menu.");
+const pchar itemDescription[] = _("Item options.");
+const pchar giveItemDescription[] = _("Give a hold item.");
+const pchar takeItemDescription[] = _("Take the hold item.");
+const pchar mailDescription[] = _("Mail options.");
+const pchar takeMailDescription[] = _("Take away mail.");
+const pchar readMailDescription[] = _("Read mail.");
+const pchar cancelSubMenuDescription[] = _("Cancel sub menu.");
+const pchar shiftDescription[] = _("Switch in.");
+const pchar sendOutDescription[] = _("Send pokémon out.");
+const pchar enterDescription[] = _("Enter pokémon.");
+const pchar noEntryDescription[] = _("Cannot enter.");
+const pchar storeDescription[] = _("Store pokémon.");
+const pchar registerDescription[] = _("Register pokémon.");
+const pchar tradeDescription[] = _("Trade pokémon.");
+const pchar nullDescription[] = _("Trade pokémon.");
+
+const pchar flashDescription[] = _("Light up darkness.");
+const pchar cutDescription[] = _("Cut a tree or grass.");
+const pchar flyDescription[] = _("Fly to a known town.");
+const pchar surfDescription[] = _("Travel on water.");
+const pchar strengthDescription[] = _("Move heavy boulders.");
+const pchar rockSmashDescription[] = _("Shatter a cracked rock.");
+const pchar waterfallDescription[] = _("Climb a waterfall.");
+const pchar teleportDescription[] = _("Return to a healing spot.");
+const pchar digDescription[] = _("Escape from here.");
+const pchar milkDrinkDescription[] = _("Share HP.");
+const pchar softboiledDescription[] = _("Share HP.");
+const pchar sweetScentDescription[] = _("Lure wild pokémon.");
+const pchar diveDescription[] = _("Dive below water.");
