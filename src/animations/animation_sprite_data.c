1f53045247f6e030705db517b167f2aee3130bab
EternalCode <dilshan.gunasekera@mail.utoronto.ca>
2019-03-12 17:27:19 -0400

    Vicegrip

--- src/animations/animation_sprite_data.c
+++ src/animations/animation_sprite_data.c
@@ -32,21 +32,21 @@
 
 
 /* the impact sprite from tackle */
-ASSETS(impact1, 4 * 4, 400);
+ASSETS(impact1, s32x32, 400);
 
 MAKE_OAM(impact1)
     OAM_PRIORITY(3)
-    OAM_SIZE(2)
+    OAM_SIZE(o32x32)
 END_OAM()
 
 
 /* the burn animation sprite */
-ASSETS(burn, 2 * 4, 401);
+ASSETS(burn, s16x32, 401);
 
 MAKE_OAM(burn)
     OAM_AFFINE()
     OAM_SHAPE(TALL)
-    OAM_SIZE(2)
+    OAM_SIZE(o16x32)
     OAM_PRIORITY(1)
 END_OAM()
 
@@ -57,39 +57,39 @@ END_AFFINE()
 
 
 /* poison ailment sprite */
-ASSETS(poison, 2 * 2, 402);
+ASSETS(poison, s16x16, 402);
 
 MAKE_OAM(poison)
-    OAM_SIZE(1)
+    OAM_SIZE(o16x16)
     OAM_PRIORITY(1)
 END_OAM()
 
 
 /* paralyze ailment sprite */
-ASSETS(paralyze, 4 * 4 * 3, 403);
+ASSETS(paralyze, s32x32 * 3, 403);
 
 MAKE_OAM(paralyze)
-    OAM_SIZE(2)
+    OAM_SIZE(o32x32)
     OAM_PRIORITY(1)
 END_OAM()
 
 BEGIN_FRAME_ANIM(paralyzeFrames)
-    SHOW_FRAME(0, 8, 5)
-    SHOW_FRAME(1, 8, 5)
-    SHOW_FRAME(4, 4, 5)
-    SHOW_FRAME(5, 4, 5)
-    SHOW_FRAME(6, 4, 5)
-    SHOW_FRAME(8, 4, 5)
+    SHOW_FRAME(0, s32x16, NOFLIP, 5)
+    SHOW_FRAME(1, s32x16, NOFLIP, 5)
+    SHOW_FRAME(4, s32x8, NOFLIP, 5)
+    SHOW_FRAME(5, s32x8, NOFLIP, 5)
+    SHOW_FRAME(6, s32x8, NOFLIP, 5)
+    SHOW_FRAME(8, s32x8, NOFLIP, 5)
     JUMP_FRAME(0)
 END_ANIM()
 
 
 /* freeze ailment sprite */
-ASSETS(freeze, 4 * 4, 404);
+ASSETS(freeze, s32x32, 404);
 
 MAKE_OAM(freeze)
     OAM_PRIORITY(3)
-    OAM_SIZE(2)
+    OAM_SIZE(o32x32)
     OAM_AFFINE()
 END_OAM()
 
@@ -108,36 +108,36 @@ END_AFFINE()
 
 
 /* confused sprite */
-ASSETS(confused, 2 * 2 * 3, 405);
+ASSETS(confused, s16x16 * 3, 405);
 
 MAKE_OAM(confused)
     OAM_PRIORITY(1)
-    OAM_SIZE(1)
+    OAM_SIZE(o16x16)
 END_OAM()
 
 BEGIN_FRAME_ANIM(confusedFrames)
-    SHOW_FRAME(0, 4, 20)
-    SHOW_FRAME(1, 4, 20)
-    SHOW_FRAME(2, 4, 20)
+    SHOW_FRAME(0, s16x16, NOFLIP, 20)
+    SHOW_FRAME(1, s16x16, NOFLIP, 20)
+    SHOW_FRAME(2, s16x16, NOFLIP, 20)
     JUMP_FRAME(0)
 END_ANIM()
 
 
 /* infactuation sprite */
-ASSETS(infactuation, 2 * 2, 406);
+ASSETS(infactuation, s16x16, 406);
 
 MAKE_OAM(infactuation)
-    OAM_SIZE(1)
+    OAM_SIZE(o16x16)
     OAM_PRIORITY(1)
 END_OAM()
 
 
 /* Sleep sprite */
-ASSETS(sleep, 4 * 4, 407);
+ASSETS(sleep, s32x32, 407);
 
 MAKE_OAM(sleep)
     OAM_AFFINE()
-    OAM_SIZE(2)
+    OAM_SIZE(o32x32)
     OAM_PRIORITY(1)
 END_OAM()
 
@@ -153,26 +153,26 @@ END_AFFINE()
 
 
 /* Smoke sprite */
-ASSETS(smoke, 4 * 4 * 3, 408);
+ASSETS(smoke, s32x32 * 3, 408);
 
 MAKE_OAM(smoke)
-    OAM_SIZE(2)
+    OAM_SIZE(o32x32)
     OAM_PRIORITY(1)
 END_OAM()
 
 BEGIN_FRAME_ANIM(smokeFrames)
-    SHOW_FRAME(0, 16, 5)
-    SHOW_FRAME(1, 16, 5)
-    SHOW_FRAME(2, 16, 5)
+    SHOW_FRAME(0, s32x32, NOFLIP, 5)
+    SHOW_FRAME(1, s32x32, NOFLIP, 5)
+    SHOW_FRAME(2, s32x32, NOFLIP, 5)
 END_ANIM()
 
 
 /* glowball sprite */
-ASSETS(glowball, 2 * 2, 409);
+ASSETS(glowball, s16x16, 409);
 
 MAKE_OAM(glowball)
     OAM_AFFINE()
-    OAM_SIZE(1)
+    OAM_SIZE(o16x16)
     OAM_PRIORITY(1)
 END_OAM()
 
@@ -193,23 +193,23 @@ END_AFFINE()
 
 
 /* pokeball sprite */
-ASSETS(pokeball, 2 * 2 * 3, 410);
+ASSETS(pokeball, s16x16 * 3, 410);
 
 MAKE_OAM(pokeball)
     OAM_AFFINE()
-    OAM_SIZE(1)
+    OAM_SIZE(o16x16)
     OAM_PRIORITY(1)
 END_OAM()
 
 const struct Frame pokeballOpen[] = {
-    SHOW_FRAME(0, 4, 5)
-    SHOW_FRAME(1, 4, 5)
+    SHOW_FRAME(0, s16x16, NOFLIP, 5)
+    SHOW_FRAME(1, s16x16, NOFLIP, 5)
     STOP_ANIM()
 };
 
 const struct Frame pokeballClose[] = {
-    SHOW_FRAME(1, 4, 5)
-    SHOW_FRAME(0, 4, 5)
+    SHOW_FRAME(1, s16x16, NOFLIP, 5)
+    SHOW_FRAME(0, s16x16, NOFLIP, 5)
     STOP_ANIM()
 };
 
@@ -241,67 +241,67 @@ END_AFFINE()
 
 
 /* impact2 sprite */
-ASSETS(impact2, 4 * 4, 411);
+ASSETS(impact2, s16x16, 411);
 
 MAKE_OAM(impact2)
-    OAM_SIZE(2)
+    OAM_SIZE(o32x32)
     OAM_PRIORITY(3)
 END_OAM()
 
 
 /* Pokeball particles when opened or closed */
-ASSETS(pokeballParticle, 1 * 1 * 3, 412);
+ASSETS(pokeballParticle, s8x8 * 3, 412);
 
 MAKE_OAM(pokeballParticle)
     OAM_PRIORITY(1)
 END_OAM()
 
 BEGIN_FRAME_ANIM(pokeballParticleFrames)
-    SHOW_FRAME(0, 1, 5)
-    SHOW_FRAME(1, 1, 5)
-    SHOW_FRAME(2, 1, 5)
+    SHOW_FRAME(0, s8x8, NOFLIP, 5)
+    SHOW_FRAME(1, s8x8, NOFLIP, 5)
+    SHOW_FRAME(2, s8x8, NOFLIP, 5)
     JUMP_FRAME(0)
 END_ANIM()
 
 
 /* Pokeball capture sucess stars particle */
-ASSETS(starParticle, 1 * 1, 413);
+ASSETS(starParticle, s8x8, 413);
 
 MAKE_OAM(starParticle)
 END_OAM()
 
 
 /* Yes no choice box for..whatever choices */
-ASSETS(yesno, 8 * 8, 414);
+ASSETS(yesno, s64x64, 414);
 
 MAKE_OAM(yesno)
-    OAM_SIZE(3)
+    OAM_SIZE(o64x64)
     OAM_PRIORITY(1)
 END_OAM()
 
 
 /* a cursor meant for the yesno box */
-ASSETS(yesnoCursor, 8 * 8, 415);
+ASSETS(yesnoCursor, s16x16, 415);
 
 MAKE_OAM(yesnoCursor)
-    OAM_SIZE(1)
+    OAM_SIZE(o16x16)
     OAM_PRIORITY(0)
 END_OAM()
 
 
 /* fist sprite */
-ASSETS(fist, 4 * 4, 416);
+ASSETS(fist, s32x32, 416);
 
 MAKE_OAM(fist)
     OAM_PRIORITY(3)
     OAM_AFFINE()
-    OAM_SIZE(2)
+    OAM_SIZE(o32x32)
 END_OAM()
 
 MAKE_OAM(fist2)
     OAM_PRIORITY(1)
     OAM_AFFINE()
-    OAM_SIZE(2)
+    OAM_SIZE(o32x32)
 END_OAM ()
 
 BEGIN_AFFINE_ANIM(fist)
@@ -320,12 +320,12 @@ END_AFFINE()
 
 
 /* circular sprite */
-ASSETS(circular, 2 * 2, 417);
+ASSETS(circular, s16x16, 417);
 
 MAKE_OAM(circular)
     OAM_AFFINE()
     OAM_PRIORITY(3)
-    OAM_SIZE(1)
+    OAM_SIZE(o16x16)
 END_OAM()
 
 BEGIN_AFFINE_ANIM(circular)
@@ -334,69 +334,69 @@ END_AFFINE()
 
 
 /* the impact sprite from tackle - but white for blendability */
-ASSETS(bimpact1, 4 * 4, 418);
+ASSETS(bimpact1, s32x32, 418);
 
 MAKE_OAM(bimpact1)
-    OAM_SIZE(2)
+    OAM_SIZE(o32x32)
     OAM_PRIORITY(3)
 END_OAM()
 
 
 /* a splash of dispersing water */
-ASSETS(watersplash, 8 * 8 * 4, 419);
+ASSETS(watersplash, s64x64 * 4, 419);
 
 MAKE_OAM(watersplash)
-    OAM_SIZE(3)
+    OAM_SIZE(o64x64)
     OAM_PRIORITY(2)
 END_OAM()
 
 BEGIN_FRAME_ANIM(watersplashFrames)
-    SHOW_FRAME(0, 64, 5)
-    SHOW_FRAME(1, 64, 5)
-    SHOW_FRAME(2, 64, 5)
-    SHOW_FRAME(3, 64, 5)
+    SHOW_FRAME(0, s64x64, NOFLIP, 5)
+    SHOW_FRAME(1, s64x64, NOFLIP, 5)
+    SHOW_FRAME(2, s64x64, NOFLIP, 5)
+    SHOW_FRAME(3, s64x64, NOFLIP, 5)
     JUMP_FRAME(0)
 END_ANIM()
 
 
 /* Chop hand from Karate chop */
-ASSETS(chop, 4 * 4 * 2, 420);
+ASSETS(chop, s32x32 * 2, 420);
 
 MAKE_OAM(chop)
-    OAM_SIZE(2)
+    OAM_SIZE(o32x32)
     OAM_PRIORITY(1)
 END_OAM()
 
 BEGIN_FRAME_ANIM(chopLeftFrames)
-    SHOW_FRAME(1, 16, 1)
+    SHOW_FRAME(1, 16, NOFLIP, 1)
 END_ANIM()
 
 
 /* Slap hand from double slap */
-ASSETS(slap, 4 * 4 * 3, 421);
+ASSETS(slap, s32x32 * 3, 421);
 
 MAKE_OAM(slap)
-    OAM_SIZE(2)
+    OAM_SIZE(o32x32)
     OAM_PRIORITY(3)
 END_OAM()
 
 BEGIN_FRAME_ANIM(slapFrames)
-    SHOW_FRAME(0, 16, 12)
-    SHOW_FRAME(1, 16, 4)
-    SHOW_FRAME(2, 16, 12)
-    SHOW_FRAME(2, 16, 12)
-    SHOW_FRAME(1, 16, 4)
-    SHOW_FRAME(0, 16, 12)
+    SHOW_FRAME(0, s32x32, NOFLIP, 12)
+    SHOW_FRAME(1, s32x32, NOFLIP, 4)
+    SHOW_FRAME(2, s32x32, NOFLIP, 12)
+    SHOW_FRAME(2, s32x32, NOFLIP, 12)
+    SHOW_FRAME(1, s32x32, NOFLIP, 4)
+    SHOW_FRAME(0, s32x32, NOFLIP, 12)
 END_ANIM()
 
 
 /* Big red fist from mega punch, and blue fist from ice punch */
-ASSETS(bigfist, 8 * 8, 422);
-ASSETS(bigbluefist, 8 * 8, 424);
+ASSETS(bigfist, s64x64, 422);
+ASSETS(bigbluefist, s64x64, 424);
 
 MAKE_OAM(bigfist)
     OAM_AFFINE()
-    OAM_SIZE(3)
+    OAM_SIZE(o64x64)
     OAM_PRIORITY(2)
 END_OAM()
 
@@ -406,26 +406,26 @@ END_AFFINE()
 
 
 /* The small fire from fire punch */
-ASSETS(smallfire, 2 * 4 * 4, 423);
+ASSETS(smallfire, s16x32 * 4, 423);
 
 MAKE_OAM(smallfire)
     OAM_SHAPE(TALL)
-    OAM_SIZE(2)
+    OAM_SIZE(o16x32)
     OAM_PRIORITY(2)
 END_OAM()
 
 MAKE_OAM(smallfireAff)
     OAM_AFFINE()
     OAM_SHAPE(TALL)
-    OAM_SIZE(2)
+    OAM_SIZE(o16x32)
     OAM_PRIORITY(3)
 END_OAM()
 
 BEGIN_FRAME_ANIM(smallfireFrames)
-    SHOW_FRAME(0, 8, 5)
-    SHOW_FRAME(1, 8, 5)
-    SHOW_FRAME(2, 8, 5)
-    SHOW_FRAME(3, 8, 5)
+    SHOW_FRAME(0, s16x32, NOFLIP, 5)
+    SHOW_FRAME(1, s16x32, NOFLIP, 5)
+    SHOW_FRAME(2, s16x32, NOFLIP, 5)
+    SHOW_FRAME(3, s16x32, NOFLIP, 5)
     JUMP_FRAME(0)
 END_ANIM()
 
@@ -435,27 +435,27 @@ END_AFFINE()
 
 
 /* Lightning bolt */
-ASSETS(lightningbolt, 4 * 4 * 2, 425);
+ASSETS(lightningbolt, s32x32 * 2, 425);
 
 MAKE_OAM(lightningbolt)
     OAM_AFFINE()
-    OAM_SIZE(2)
+    OAM_SIZE(o32x32)
     OAM_PRIORITY(3)
 END_OAM()
 
 BEGIN_FRAME_ANIM(lightningboltFrames)
-    SHOW_FRAME(0, 16, 2)
-    SHOW_FRAME(1, 16, 2)
+    SHOW_FRAME(0, s32x32, NOFLIP, 2)
+    SHOW_FRAME(1, s32x32, NOFLIP, 2)
     JUMP_FRAME(0)
 END_ANIM()
 
 
 /* Yellow fist sprite from thunder punch */
-ASSETS(yellowfist, 8 * 8, 426);
+ASSETS(yellowfist, s64x64, 426);
 
 MAKE_OAM(yellowfist)
     OAM_AFFINE()
-    OAM_SIZE(3)
+    OAM_SIZE(o64x64)
 END_OAM()
 
 BEGIN_AFFINE_ANIM(bigfistShrinkSlowly)
@@ -466,33 +466,28 @@ END_AFFINE()
 
 
 /* Vicegrip graphic */
-ASSETS(vicegrip, 4 * 4, 427);
+ASSETS(vicegrip, s32x32 * 3, 427);
 
 MAKE_OAM(vicegrip)
-    OAM_PRIORITY(1)
-    OAM_SIZE(2)
+    OAM_PRIORITY(2)
+    OAM_SIZE(o32x32)
 END_OAM()
 
+MAKE_OAM(vicegripFlip)
+    OAM_PRIORITY(2)
+    OAM_HFLIP()
+    OAM_VFLIP()
+    OAM_SIZE(o32x32)
+END_OAM()
 
+BEGIN_FRAME_ANIM(vicegripFrame)
+    SHOW_FRAME(0, s32x32, NOFLIP, 2)
+    SHOW_FRAME(1, s32x32, NOFLIP, 2)
+    SHOW_FRAME(2, s32x32, NOFLIP, 2)
+END_ANIM()
 
-/*
-	OAM DATA:
-
-    // priority 3 = all battlers
-    // priority 2 = hp boxes
-    // priority 1 free
-    // priortiy 0 free
-	priority: 0-3; 0=Highest
-
-	affine_mode: 0=Normal, 1=Affine
-	h_flip 1=flip horizontally
-    v_flip 1=flip vertically
-
-	shape: 0=Square,1=Horizontal,2=Vertical
-	size:
-     * Size  Square   Horizontal  Vertical
-     * 0     8x8      16x8        8x16
-     * 1     16x16    32x8        8x32
-     * 2     32x32    32x16       16x32
-     * 3     64x64    64x32       32x64
-*/
+BEGIN_FRAME_ANIM(vicegripFlipFrame)
+    SHOW_FRAME(0, s32x32, HVFLIP, 2)
+    SHOW_FRAME(1, s32x32, HVFLIP, 2)
+    SHOW_FRAME(2, s32x32, HVFLIP, 2)
+END_ANIM()
--- src/animations/moves/vicegrip.s
+++ src/animations/moves/vicegrip.s
@@ -3,9 +3,46 @@
 .align 2
 .include "src/animation_cmds.s"
 
+.equ gripTop, 0x9006
+.equ gripBot, 0x9007
+.equ impactParticle, 0x9008
 
 .global VicegripAnimation
 VicegripAnimation:
+    fastsetbattlers
+    loadspritefull vicegripSprite vicegripPalette vicegripOam
+    copyvar gripTop LASTRESULT
+    loadspritefull impact1Sprite impact1Palette impact1Oam
+    copyvar impactParticle LASTRESULT
+    loadsprite vicegripSprite vicegripPalette vicegripFlipOam
+    copyvar gripBot LASTRESULT
+    spritetobg target 8 8
+    spriteblend 4 12
+    BLOCKCMD
+    setframessprite 0 gripTop vicegripFrame
+    setframessprite 0 gripBot vicegripFlipFrame
+    addvar targetx 24
+    subvar targety 24
+    rendersprite gripTop targetx targety nullrsf
+    subvar targetx 48
+    addvar targety 48
+    rendersprite gripBot targetx targety nullrsf
+    fastsetbattlers
+    OPENCMD
+    movesprite gripTop 0xFFFF 1 8 false
+    movesprite gripBot 1 0xFFFF 8 true
+    wait
+    BLOCKCMD
+    rendersprite impactParticle targetx targety nullrsf
+    quakebg 1 2 0 2 2 false
+    setvar gLASTRESULT 0x57FF
+    startscript FireworkAnimation true
+    OPENCMD
+    pauseframes 10
+    deletesprite impactParticle
+    pauseframes 5
+    deletesprite gripTop
+    deletesprite gripBot
     end
 
 .pool
