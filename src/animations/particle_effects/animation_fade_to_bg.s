6d184f69b8b41482fc9462d95c5cabac3365a664
EternalCode <dilshan.gunasekera@mail.utoronto.ca>
2019-03-23 21:54:23 -0400

    fadetobg animation script uses variables and has a partial fade option

--- src/animations/particle_effects/animation_fade_to_bg.s
+++ src/animations/particle_effects/animation_fade_to_bg.s
@@ -6,16 +6,20 @@
 .equ coA, 0x9010
 .equ coB, 0x9011
 .equ speed, 0x9012
+.equ amount, 0x9013
 
 // You must set the target to the BG before calling this
 // Additionally, set the CoefficientA, CoefficientB and speed to starting values
 
 .global FadeToBGAnimation
 FadeToBGAnimation:
+    BLOCKCMD
     blendsemitransparent coA coB
+    setvar amount 16
+    OPENCMD
 
 fadeLoop:
-    compare coB 16
+    comparevars coB amount
     BLOCKCMD
     if1 0x1 goto finish
     addvar coB speed
@@ -27,5 +31,26 @@ fadeLoop:
 finish:
     OPENCMD
     end
+
+
+
+.global FadeToBGAmountAnimation
+FadeToBGAmountAnimation:
+    blendsemitransparent coA coB
+    goto fadeLoop
+
+.global FadeFromBgAmountAnimation
+FadeFromBgAmountAnimation:
+    blendsemitransparent coA coB
+
+fadeLoopBackwards:
+    comparevars coB amount
+    BLOCKCMD
+    if1 0x1 goto finish
+    subvar coB speed
+    addvar coA speed
+    blendsemitransparent coA coB
+    OPENCMD
+    goto fadeLoopBackwards
 
 .pool
