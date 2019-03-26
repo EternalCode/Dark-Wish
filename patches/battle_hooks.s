d414bc21f9af77c08127023635abf18a7616b454
EternalCode <dilshan.gunasekera@mail.utoronto.ca>
2019-03-25 20:03:42 -0400

    test data applied to vanilla trainer battles

--- patches/battle_hooks.s
+++ patches/battle_hooks.s
@@ -7,3 +7,10 @@
 .org 0x083DB028 + (0x2C * 4) + 0x24
     .word PokeballTask|1;
 .pool
+
+
+// opponent trainer party test hook
+.org 0x080112E0
+    ldr r0, =(TestBattleDataInit|1)
+    bx r0
+.pool
